9b028dc59c8db05db54a59c2194f5c7b4db181c1
Swellington Soares <swellington.santos@gmail.com>
2022-11-21 16:51:22 -0300

    cinematiccam

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'Kiminaze'
+
+client_scripts {
+	-- '@NativeUILua_Reloaded/src/NativeUIReloaded.lua',	
+	"NativeUI.lua",
+	'config.lua',
+	'client.lua'
+}
+
+depedencies {
+	"NativeUILua_Reloaded"
+}
+--server_script 'server.lua'
